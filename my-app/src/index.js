9c6e470bdd1938951c7ddc42bb00e2f5f907c98a
enzorooo <ph.enzomercado@gmail.com>
2021-12-24 21:59:30 +0800

    Adding first reactjs app

---
+++ my-app/src/index.js
@@ -0,0 +1,36 @@
+// import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// Enable live reloading
+if (module.hot) {
+    module.hot.accept();
+  }
+
+
+// Functions
+  
+// Create a react component
+const App = () => {
+    const buttonText = {text: 'Click Me'};
+    const labelText = 'Enter Name:';
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                {labelText}
+            </label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor:'blue', color:'white'}}>
+                {buttonText.text}
+            </button>
+        </div>
+    );
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
