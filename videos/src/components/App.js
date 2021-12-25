6c3f3e4b1a8ba9990bbff16a5ea633d984742828
enzorooo <ph.enzomercado@gmail.com>
2021-12-26 02:24:41 +0800

    finished videos reactjs project

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
+
+    componentDidMount() {
+        this.onSearchSubmit("react js");
+    }
 
     onSearchSubmit = async (term) => {
         var endpoint = '/search'; 
@@ -11,15 +17,37 @@ class App extends React.Component {
             params: {q: term}
         });
 
-        this.setState({videos: response.data.items});
+        this.setState({
+            videos: response.data.items,
+
+            // set the first video as the default selected video
+            selectedVideo: response.data.items[0]
+        });
 
     };
+
+    onVideoSelect = (video) => {
+        console.log('[APP] video selected: ', video);
+        this.setState({selectedVideo: video});
+    }
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onSearchSubmit={this.onSearchSubmit} />
-                I have {this.state.videos.length} videos.
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList 
+                                onVideoSelect={this.onVideoSelect} 
+                                videos={this.state.videos} 
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     };
